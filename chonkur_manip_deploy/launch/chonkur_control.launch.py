2c17ee099e2e7161ce7411b5a71d439303a8d717
Emma Zemler <emma.zemler@nasa.gov>
2023-06-16 15:32:22 -0500

    reworking to include a deploy package

---
+++ chonkur_manip_deploy/launch/chonkur_control.launch.py
@@ -0,0 +1,107 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir
+import os
+
+def generate_launch_description():
+
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value='""',
+            description="tf_prefix of the joint names, useful for \
+        multi-robot setup. If changed, also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "headless_mode",
+            default_value="false",
+            description="Enable headless mode for robot control",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "fake_sensor_commands",
+            default_value="false",
+            description="Enable fake command interfaces for sensors used for simple simulations. \
+            Used only if 'use_fake_hardware' parameter is true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "initial_joint_controller",
+            default_value="joint_trajectory_controller",
+            description="Initially loaded robot controller.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "activate_joint_controller",
+            default_value="true",
+            description="Activate loaded joint controller.",
+        )
+    )
+
+    tf_prefix = LaunchConfiguration("tf_prefix")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    headless_mode = LaunchConfiguration("headless_mode")
+    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
+    initial_joint_controller = LaunchConfiguration("initial_joint_controller")
+    activate_joint_controller = LaunchConfiguration("activate_joint_controller")    
+
+
+    base_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory("ur_robot_driver"), 'launch','ur_control.launch.py')),
+        launch_arguments={
+            "ur_type": "ur10e",
+            "robot_ip": "192.168.1.102",
+            "description_package": "chonkur_manip_description",
+            "description_file": "chonkur.urdf.xacro",
+            "tf_prefix": tf_prefix,
+            "use_fake_hardware": use_fake_hardware,
+            "headless_mode": headless_mode,
+            "fake_sensor_commands": fake_sensor_commands,
+            "initial_joint_controller": initial_joint_controller,
+            "activate_joint_controller": activate_joint_controller,
+        }.items(),
+    )
+
+    gripper_controller_yaml = os.path.join(get_package_share_directory("robotiq_driver"), 'config','robotiq_hande_controllers.yaml')
+    gripper_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["robotiq_gripper_hande_controller", 
+                   "-c", "controller_manager",
+                   "-t", "position_controllers/GripperActionController",
+                   "-p", gripper_controller_yaml
+                  ]
+    )
+
+    gripper_activation_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["robotiq_activation_controller", 
+                   "-c", "controller_manager",
+                   "-t", "robotiq_controllers/RobotiqActivationController",
+                   "-p", gripper_controller_yaml
+                  ]
+    )    
+
+    nodes = [gripper_controller_spawner, gripper_activation_controller_spawner]
+
+    return LaunchDescription(declared_arguments + [base_launch] + nodes)
