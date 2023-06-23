0cf10004102b93c3b1b3d020cf2e79f3d86e9380
Emma Zemler <emma.zemler@nasa.gov>
2023-06-23 16:51:10 -0500

    fixed a bug preventing moveit from working

--- clr_moveit_config/launch/clr_moveit.launch.py
+++ clr_moveit_config/launch/clr_moveit.launch.py
@@ -15,14 +15,17 @@ def generate_launch_description():
 
     description_package = 'clr_description'
     description_file = 'clr.urdf.xacro'
+    description_full_path = os.path.join(get_package_share_directory(description_package), 'urdf',description_file)
     moveit_config_package = 'clr_moveit_config'
-    moveit_config_srdf_file = 'clr.srdf.xacro'
+    moveit_config_srdf_file = 'clr.srdf'
+
 
     moveit_config = (
-        MoveItConfigsBuilder("clr")
-        .robot_description(file_path="/home/ndunkelb/clr_ws/install/clr_description/share/clr_description/urdf/clr.urdf.xacro")
-        .robot_description_semantic(file_path="config/" + moveit_config_srdf_file)
-        .robot_description_kinematics(file_path="config/" + "kinematics.yaml")
+        MoveItConfigsBuilder("clr", package_name="clr_moveit_config")
+        .robot_description(file_path=description_full_path)
+        .robot_description_semantic(file_path=os.path.join(get_package_share_directory(moveit_config_package), 'srdf',moveit_config_srdf_file))
+        .robot_description_kinematics(file_path=os.path.join(get_package_share_directory(moveit_config_package), 'config',"kinematics.yaml"))
+        .joint_limits(file_path=os.path.join(get_package_share_directory(moveit_config_package), 'config',"joint_limits.yaml"))
         .to_moveit_configs()
     )
 
@@ -48,88 +51,15 @@ def generate_launch_description():
     use_fake_hardware = LaunchConfiguration("use_fake_hardware")
     tf_prefix = LaunchConfiguration("tf_prefix")
 
-    # robot_description_content = Command(
-    #     [
-    #         PathJoinSubstitution([FindExecutable(name="xacro")]),
-    #         " ",
-    #         PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
-    #         " ",        
-    #         "use_fake_hardware:=",
-    #         use_fake_hardware,
-    #         " ",
-    #         "tf_prefix:=",
-    #         tf_prefix,
-    #         " ",
-    #     ]
-    # )
-    # robot_description = {"robot_description": robot_description_content}
+    print(moveit_config.robot_description_kinematics)
 
-    # MoveIt Configuration
-    # robot_description_semantic_content = Command(
-    #     [
-    #         PathJoinSubstitution([FindExecutable(name="xacro")]),
-    #         " ",
-    #         PathJoinSubstitution(
-    #             [FindPackageShare(moveit_config_package), "srdf", moveit_config_srdf_file]
-    #         ), 
-    #         " ", 
-    #         "name:=",
-    #         "clr",
-    #         " ",               
-    #     ]
-    # )
-    # robot_description_semantic = {"robot_description_semantic": robot_description_semantic_content}
-
-    robot_description_kinematics = PathJoinSubstitution(
-        [FindPackageShare(moveit_config_package), "config", "kinematics.yaml"]
-    )
-
-    # Planning Configuration
-    # ompl_planning_pipeline_config = {
-    #     "move_group": {
-    #         "planning_plugin": "ompl_interface/OMPLPlanner",
-    #         "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
-    #         "start_state_max_bounds_error": 0.1,
-    #     }
-    # }
-    # ompl_planning_yaml = load_yaml(moveit_config_package, "config/ompl_planning.yaml")
-    # ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
-
-    # # Trajectory Execution Configuration
-    # moveit_controllers = {
-    #     "moveit_controller_manager": "moveit_ros_control_interface/Ros2ControlManager",
-    # }
-
-    # trajectory_execution = {
-    #     "moveit_manage_controllers": False,
-    #     "trajectory_execution.allowed_execution_duration_scaling": 1.2,
-    #     "trajectory_execution.allowed_goal_duration_margin": 0.5,
-    #     "trajectory_execution.allowed_start_tolerance": 0.01,
-    # }
-
-    planning_scene_monitor_parameters = {
-        "publish_planning_scene": True,
-        "publish_geometry_updates": True,
-        "publish_state_updates": True,
-        "publish_transforms_updates": True,
-    }
-
-    # warehouse_ros_config = {
-    #     "warehouse_plugin": "warehouse_ros_sqlite::DatabaseConnection",
-    #     "warehouse_host": os.path.expanduser("~/.ros/warehouse_ros.sqlite"),
-    # }
-
-
-    # # joint limits
-    # joint_limits_yaml = load_yaml(moveit_config_package, "config/joint_limits.yaml")
-    # joint_limits = {'robot_description_planning': joint_limits_yaml}
-
-    # Start the actual move_group node/action server
     move_group_node = Node(
         package="moveit_ros_move_group",
         executable="move_group",
         output="screen",
-        parameters=[moveit_config.to_dict()]
+        parameters=[
+            moveit_config.to_dict(),         
+            ]
     )
 
     # rviz with moveit configuration
@@ -143,25 +73,17 @@ def generate_launch_description():
         output="log",
         arguments=["-d", rviz_config_file],
         parameters=[
-            # robot_description,
-            # robot_description_semantic,
-            # ompl_planning_pipeline_config,
-            # robot_description_kinematics,
-            # joint_limits,
-            # warehouse_ros_config,
             moveit_config.robot_description,
             moveit_config.robot_description_semantic,
             moveit_config.robot_description_kinematics,
             moveit_config.planning_pipelines,
             moveit_config.joint_limits,
-            robot_description_kinematics,
-            planning_scene_monitor_parameters,
-            # warehouse_ros_config
+            moveit_config.planning_scene_monitor,
         ],
     )    
 
 
-
+    # TODO: turn move group and rviz node as optional
     nodes_to_start = [
         move_group_node, 
         rviz_node
