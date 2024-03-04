6dadec4b121192eb849e0790b2a7cd07fb3c35d1
Mike Tobia <michael.e.tobia@nasa.gov>
2024-03-04 17:29:47 -0600

    active option doesn't exist, removing the inactive is correct, fixed

--- chonkur_deploy/launch/spawn_controllers.launch.py
+++ chonkur_deploy/launch/spawn_controllers.launch.py
@@ -26,7 +26,8 @@ def generate_launch_description():
                    "-c", "controller_manager",
                    "-t", "admittance_controller/AdmittanceController",
                 #    "-p", controller_params_file,
-                   "--active"],
+                #    "--inactive",
+                   ],
     )
     admittance_jtc_spawner = Node(
         package="controller_manager",
@@ -36,7 +37,8 @@ def generate_launch_description():
                    "-c", "controller_manager",
                    "-t", "joint_trajectory_controller/JointTrajectoryController ",
                 #    "-p", controller_params_file,
-                   "--active"],
+                #    "--inactive",
+                   ],
     )    
 
     nodes = [admittance_controller_spawner, 
