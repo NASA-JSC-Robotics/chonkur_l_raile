c12c21c719d22207de6363f8c10cbd472f0c7653
Mike Tobia <michael.e.tobia@nasa.gov>
2024-03-11 18:33:28 -0500

    starting work on controller status node

---
+++ drt_ros2_control_tools/ctrl_stat_tools.py
@@ -0,0 +1,55 @@
+import os
+import yaml
+
+import rclpy
+from rclpy.node import Node
+
+
+from ament_index_python.packages import get_package_share_directory
+
+from controller_manager_msgs import ListControllers
+
+class ControlStatusClient(Node):
+    def __init__(self):
+        super().__init__('ctrl_status_client')
+
+        # IDK how to do either of these
+        # TODO: Specify hardware/sim_controllers.yaml via hardware/sim operation
+        # TODO: Specify cfg path by running robot
+        self.ctrlr_cfg_path = os.path.join(get_package_share_directory('clr_deploy'), 'config', 'sim_controllers.yaml')
+
+        self.list_ctrlrs = self.create_client(ListControllers, '/controller_manager/list_controllers')
+        while not self.list_ctrlrs.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info(f'{self.list_ctrlrs.srv_name} is not available, waiting again...')
+        self.list_ctrlrs_req = ListControllers.Request()
+
+    def get_spawned_ctrlr_names(self):
+        self.future = self.list_ctrlrs.call_async(self.list_ctrlrs_req)
+        rclpy.spin_until_future_complete(self, self.future)
+        self.spawned_ctrlr_names = [item.name for item in self.future.result.controller]
+        return self.spawned_ctrlr_names
+    
+    def get_all_ctrlr_names(self):
+        # TODO: If ctrlr_cfg_path doesn't exist, fail return exit
+        with open(self.ctrlr_cfg_path, 'r') as file:
+            ctrlr_cfg = yaml.safe_load(file)
+        self.all_ctrlr_names = list(ctrlr_cfg['controller_manager']['ros__parameters'].keys())
+        return self.all_ctrlr_names
+    
+    def compare_ctrlrs(self):
+        ctrlrs_spawned = set(self.all_ctrlr_names) & set(self.spawned_ctrlr_names)
+        ctrlrs_not_spawned = set(self.all_ctrlr_names) - set(self.spawned_ctrlr_names)
+        return ctrlrs_spawned, ctrlrs_not_spawned 
+
+        
+        
+
+
+
+
+
+
+# Call list_controllers for controller status
+# Get all controllers from compiled configuration yaml
+# Compare all controllers vs spawned controllers
+# Return compare
