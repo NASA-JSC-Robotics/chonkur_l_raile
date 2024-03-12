d5cfef200b44e11a86676f3203994af595efa104
Mike Tobia <michael.e.tobia@nasa.gov>
2024-03-12 16:06:57 -0500

    ctrl_stat_tools.py runs

--- drt_ros2_control_tools/ctrl_stat_tools.py
+++ drt_ros2_control_tools/ctrl_stat_tools.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import yaml
 
@@ -7,7 +9,7 @@ from rclpy.node import Node
 
 from ament_index_python.packages import get_package_share_directory
 
-from controller_manager_msgs import ListControllers
+from controller_manager_msgs.srv import ListControllers
 
 class ControlStatusClient(Node):
     def __init__(self):
@@ -26,7 +28,7 @@ class ControlStatusClient(Node):
     def get_spawned_ctrlr_names(self):
         self.future = self.list_ctrlrs.call_async(self.list_ctrlrs_req)
         rclpy.spin_until_future_complete(self, self.future)
-        self.spawned_ctrlr_names = [item.name for item in self.future.result.controller]
+        self.spawned_ctrlr_names = [item.name for item in self.future.result().controller]
         return self.spawned_ctrlr_names
     
     def get_all_ctrlr_names(self):
@@ -37,12 +39,25 @@ class ControlStatusClient(Node):
         return self.all_ctrlr_names
     
     def compare_ctrlrs(self):
+        self.get_spawned_ctrlr_names()
+        self.get_all_ctrlr_names()
         ctrlrs_spawned = set(self.all_ctrlr_names) & set(self.spawned_ctrlr_names)
         ctrlrs_not_spawned = set(self.all_ctrlr_names) - set(self.spawned_ctrlr_names)
         return ctrlrs_spawned, ctrlrs_not_spawned 
 
-        
-        
+
+# TODO: put main in a different file
+def main():
+    rclpy.init()
+
+    ctrl_stat_client = ControlStatusClient()
+    spawned, not_spawned = ctrl_stat_client.compare_ctrlrs()
+    print(not_spawned)
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 
