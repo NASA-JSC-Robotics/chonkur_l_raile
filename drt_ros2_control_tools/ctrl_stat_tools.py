6fcfb91a7c0b769eed037e15277d9e143c480768
Mike Tobia <michael.e.tobia@nasa.gov>
2024-03-14 12:54:37 -0500

    added error checking and sim/hardware selection

--- drt_ros2_control_tools/ctrl_stat_tools.py
+++ drt_ros2_control_tools/ctrl_stat_tools.py
@@ -10,14 +10,14 @@ from ament_index_python.packages import get_package_share_directory
 from controller_manager_msgs.srv import ListControllers
 
 class ControlStatusClient(Node):
-    def __init__(self):
+    def __init__(self, sim):
         super().__init__('ctrl_status_client')
-
-        # IDK how to do either of these
-        # TODO: Specify hardware/sim_controllers.yaml via hardware/sim operation
-        # TODO: Specify cfg path by running robot
-        self.ctrlr_cfg_path = os.path.join(get_package_share_directory('clr_deploy'), 'config', 'sim_controllers.yaml')
-
+        if sim:
+            self.get_logger().info('SIM SELECTED')
+            self.ctrlr_cfg_path = os.path.join(get_package_share_directory('clr_deploy'), 'config', 'sim_controllers.yaml')
+        else:
+            self.get_logger().info('HARDWARE SELECTED')
+            self.ctrlr_cfg_path = os.path.join(get_package_share_directory('clr_deploy'), 'config', 'hardware_controllers.yaml')
         self.list_ctrlrs = self.create_client(ListControllers, '/controller_manager/list_controllers')
         while not self.list_ctrlrs.wait_for_service(timeout_sec=1.0):
             self.get_logger().info(f'{self.list_ctrlrs.srv_name} is not available, waiting again...')
@@ -31,17 +31,24 @@ class ControlStatusClient(Node):
     
     def get_all_ctrlrs(self):
         # TODO: If ctrlr_cfg_path doesn't exist, fail return exit
-        with open(self.ctrlr_cfg_path, 'r') as file:
-            ctrlr_cfg = yaml.safe_load(file)
-        self.all_ctrlrs = list(ctrlr_cfg['controller_manager']['ros__parameters'].keys())
+        if not os.path.exists(self.ctrlr_cfg_path):
+            self.get_logger().error(f"Failed to find configuration file at {self.ctrlr_cfg_path}, file does not exist")
+            self.all_ctrlrs = None
+        else:
+            with open(self.ctrlr_cfg_path, 'r') as file:
+                ctrlr_cfg = yaml.safe_load(file)
+            self.all_ctrlrs = list(ctrlr_cfg['controller_manager']['ros__parameters'].keys())
         return self.all_ctrlrs
     
     def compare_ctrlrs(self):
         self.get_spawned_ctrlrs()
         self.get_all_ctrlrs()
-        spawned_ctrlr_names = [ctrlr.name for ctrlr in self.spawned_ctrlrs]
-        spawned_ctrlr_states = [ctrlr.state for ctrlr in self.spawned_ctrlrs]
-        ctrlrs_spawned = {'name': spawned_ctrlr_names, 'state':spawned_ctrlr_states}
-        ctrlrs_not_spawned = {'name': (set(self.all_ctrlrs) - set(spawned_ctrlr_names))}
-        ctrlr_status = {'spawned': ctrlrs_spawned, 'not_spawned': ctrlrs_not_spawned}
+        if self.all_ctrlrs == None:
+            ctrlr_status = None
+        else:
+            spawned_ctrlr_names = [ctrlr.name for ctrlr in self.spawned_ctrlrs]
+            spawned_ctrlr_states = [ctrlr.state for ctrlr in self.spawned_ctrlrs]
+            ctrlrs_spawned = {'name': spawned_ctrlr_names, 'state':spawned_ctrlr_states}
+            ctrlrs_not_spawned = {'name': (set(self.all_ctrlrs) - set(spawned_ctrlr_names))}
+            ctrlr_status = {'spawned': ctrlrs_spawned, 'not_spawned': ctrlrs_not_spawned}
         return ctrlr_status
--- scripts/controller_check.py
+++ scripts/controller_check.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import rclpy
 from drt_ros2_control_tools.ctrl_stat_tools import ControlStatusClient
 from drt_ros2_control_tools.ctrl_config_compiler import bcolors
@@ -6,18 +7,28 @@ from drt_ros2_control_tools.ctrl_config_compiler import bcolors
 
 # TODO: CURRENTLY NOT WORKING, compare_ctrlrs outputs a singular dict now, adapt this script and add error behaviors to compare_ctrlrs!!
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sim",
+                        default=False,
+                        action='store_true',
+                        help="Indicate if robot is running in sim")
+    args = parser.parse_args()
     rclpy.init()
+    print(f'args.sim: {args.sim}')
 
-    ctrl_stat_client = ControlStatusClient()
+    ctrl_stat_client = ControlStatusClient(args.sim)
     ctrlr_status = ctrl_stat_client.compare_ctrlrs()
     
-    print(bcolors.OKGREEN + bcolors.BOLD + 'Listed and spawned:' + bcolors.ENDC)
-    for i in range(len(ctrlr_status['spawned']['name'])):
-        print('\t - ' + ctrlr_status['spawned']['name'][i] + ' [' + ctrlr_status['spawned']['state'][i] + ']')
-    # _ = [print('\t -' + ctrlr['name'] + ' [' + ctrlr['status'] + ']') for ctrlr in ctrlr_status['spawned']]
+    if ctrlr_status:
+        print(bcolors.OKGREEN + bcolors.BOLD + 'Listed and spawned:' + bcolors.ENDC)
+        for i in range(len(ctrlr_status['spawned']['name'])):
+            print('\t - ' + ctrlr_status['spawned']['name'][i] + ' [' + ctrlr_status['spawned']['state'][i] + ']')
+        
+        print(bcolors.WARNING + bcolors.BOLD + 'Listed and not spawned' + bcolors.ENDC)
+        _ = [print('\t -' + name) for name in ctrlr_status['not_spawned']['name']]
     
-    print(bcolors.WARNING + bcolors.BOLD + 'Listed and not spawned' + bcolors.ENDC)
-    _ = [print('\t -' + name) for name in ctrlr_status['not_spawned']['name']]
+    ctrl_stat_client.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
