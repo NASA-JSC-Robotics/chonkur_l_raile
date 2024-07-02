bb2e1920a539864926d2e386cf595d7ecfab2f47
Mina Kian <mina.j.kian@nasa.gov>
2024-07-02 14:25:00 -0500

    added a multithreaded executor and callback groups

--- drt_ros2_control_tools/mina_support_classes_name_later.py
+++ drt_ros2_control_tools/mina_support_classes_name_later.py
@@ -6,30 +6,37 @@ from std_msgs.msg import String
 from ur_dashboard_msgs.msg import SafetyMode
 from controller_manager_msgs.srv import ListControllers
 from action_msgs.srv import CancelGoal
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.callback_groups import ReentrantCallbackGroup
+
 
 
 class EStopSafety(Node):
-    def __init__(self, estop_topic):
+    def __init__(self, estop_topic, list_controllers_service_name):
         super().__init__('estop_safety')
+        self.estop_values = [7,11]
+        self.list_controllers_service_name = list_controllers_service_name
+
         self.subscription = self.create_subscription(
             SafetyMode,
             estop_topic,
             self.estop_listener_callback,
             10)
         self.subscription,  # prevent unused variable warning
-        self.have_controllers = False
+        self.pull_controllers_cb_group = MutuallyExclusiveCallbackGroup()
+        self.cancel_controllers_cb_group = ReentrantCallbackGroup() 
         
-    def estop_listener_callback(self, stop_list, msg):
-        # call get_controllers only once
-        # need to call "cancel controllers" but only once
-        # if estop + hit
-        print("here")
-        #self.cancel_controllers()
+    def estop_listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg)
+        if (msg.mode in self.estop_values):
+            # if the safety messages are triggered, get the controllers and cancel them
+            self.get_controllers()
+            self.cancel_controllers()
+            print("done cancelling actions")
     
-    def get_controllers(self, list_controllers_service_name):
+    def get_controllers(self):
         # this function returns the list of the controllers
-        self.list_ctrlrs_client = self.create_client(ListControllers, list_controllers_service_name)
+        self.list_ctrlrs_client = self.create_client(ListControllers, self.list_controllers_service_name, callback_group= self.pull_controllers_cb_group)
 
         while not self.list_ctrlrs_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info(f'{self.list_ctrlrs_client.srv_name} is not available, waiting again...')
@@ -41,13 +48,11 @@ class EStopSafety(Node):
         self.spawned_ctrlr_names = [ctrlr.name for ctrlr in self.spawned_ctrlrs]
         self.spawned_ctrlr_types = [ctrlr.type for ctrlr in self.spawned_ctrlrs]
         self.controller_names = []
-        #print(self.spawned_ctrlr_names)
-        #print(self.spawned_ctrlr_types)
         for i in range(0, len(self.spawned_ctrlr_names)):
             if(self.spawned_ctrlr_types[i] == "joint_trajectory_controller/JointTrajectoryController"):
                 self.controller_names.append(self.spawned_ctrlr_names[i])
-        #print("list of controller names .....................")
-        #print(self.controller_names)
+        print("list of controller names .....................")
+        print(self.controller_names)
 
     def cancel_controllers(self):
         # empty cancel goal request msg (works for both full robot and gripper)
@@ -62,7 +67,7 @@ class EStopSafety(Node):
 
             #print("this is the client name " + client_name)
             # instantiate a client
-            cancel_client = self.create_client(CancelGoal, client_name)
+            cancel_client = self.create_client(CancelGoal, client_name, callback_group=self.cancel_controllers_cb_group)
             while not cancel_client.wait_for_service(timeout_sec=1.0):
                 self.get_logger().info('service not available, waiting again...')
                 cancel_client.call(cancel_msg)
--- scripts/estop_safety.py
+++ scripts/estop_safety.py
@@ -3,43 +3,34 @@ import sys
 import rclpy
 from rclpy.node import Node
 from drt_ros2_control_tools.mina_support_classes_name_later import EStopSafety
-############### for testing
+from rclpy.executors import MultiThreadedExecutor 
+
 def main(args=None):
     rclpy.init(args=args)
 
     estop_t = '/io_and_status_controller/safety_mode'
-    minimal_subscriber = EStopSafety(estop_t)
-
     list_controllers_service = '/controller_manager/list_controllers'
+    #print("mina sanity check")
 
-    # getting controllers
-    print("getting controllers")
-    minimal_subscriber.get_controllers(list_controllers_service)
-    # cancelling
-    #print("cancelling controllers")
-    #minimal_subscriber.cancel_controllers()
+    try:
+        estop_node = EStopSafety(estop_t, list_controllers_service)
 
-    ######################
-    param_topic_controller_manager = '/controller_manager'
-    
-    
-    #cancel_msg = CancelGoal.Request()
-    #print(cancel_msg.goal_info)
+        executor = MultiThreadedExecutor(num_threads=10) #check number 
+        executor.add_node(estop_node)
 
+        #rclpy.spin(estop_node)
+        try: 
+            print("spinning")
+            executor.spin()
+            
 
-    # cancel_client = minimal_subscriber.create_client(CancelGoal, '/joint_trajectory_controller/follow_joint_trajectory/_action/cancel_goal')
-    # while not cancel_client.wait_for_service(timeout_sec=1.0):
-    #         minimal_subscriber.get_logger().info('service not available, waiting again...')
-    
-    # cancel_client.call(cancel_msg)
-
-    rclpy.spin(minimal_subscriber)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
-    rclpy.shutdown()
+        finally:
+            executor.shutdown()
+            estop_node.destroy_node()
+        
+    finally:
+        #estop_node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
