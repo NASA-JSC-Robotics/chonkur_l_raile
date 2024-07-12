1c04acaec04bcfc877bd046f129c22b55568320e
Mina Kian <mina.j.kian@nasa.gov>
2024-07-12 14:32:32 -0500

    code review with Emma, adjusted cmake,package xml, and further cleaned code

--- clr_control/clr_control/clr_estop_safety_class.py
+++ clr_control/clr_control/clr_estop_safety_class.py
@@ -2,9 +2,26 @@ from drt_ros2_control_tools.estop_safety_class import EStopSafety
 from ur_dashboard_msgs.msg import SafetyMode
 
 class CLREStopSafety(EStopSafety):
+    """This class cancels the CLR's controllers when the EStop has been triggered.
+
+    Args:
+        EStopSafety (Node): This class cancels the general robot's controllers when the EStop has been triggered.
+    """
     def __init__(self):
+        """the constructor for the CLREStopSafety Class, which creates a subscriber to the CLR's EStop topic.
+        """
         clr_estop_topic = '/io_and_status_controller/safety_mode'
-        self.estop_values_list = [3,5,6,7,11,12,13]
+        self.estop_values_list = [
+            SafetyMode.PROTECTIVE_STOP,
+            SafetyMode.SAFEGUARD_STOP,
+            SafetyMode.SYSTEM_EMERGENCY_STOP,
+            SafetyMode.ROBOT_EMERGENCY_STOP,
+            SafetyMode.VIOLATION,
+            SafetyMode.FAULT,
+            SafetyMode.UNDEFINED_SAFETY_MODE,
+            SafetyMode.AUTOMATIC_MODE_SAFEGUARD_STOP,
+            SafetyMode.SYSTEM_THREE_POSITION_ENABLING_STOP]
+        
         ur_estop_msg_type = SafetyMode
         include_gripper = True
         list_controllers_service = '/controller_manager/list_controllers'
@@ -12,4 +29,13 @@ class CLREStopSafety(EStopSafety):
         super().__init__(estop_topic = clr_estop_topic, estop_msg_type = ur_estop_msg_type, include_gripper = include_gripper, list_controllers_service_name = list_controllers_service)
 
     def estop_triggered(self, msg):
+        """Evaluate the estop SafetyMode msg to determine if the estop has been triggered. Returns true once the estop has been triggered and false all other times.
+
+
+        Args:
+            msg (SafetyMode): msg published on the estop topic.
+
+        Returns:
+            bool: if the estop has been triggered or not
+        """
         return msg.mode in self.estop_values_list
