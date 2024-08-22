7b222b7805ae7b181293c4617d508a220be9a977
Erik Holum <eholum@gmail.com>
2024-08-22 16:56:34 -0400

    Clean spinning and shutdown for the estopper

--- clr_control/scripts/estop_safety.py
+++ clr_control/scripts/estop_safety.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
+import sys
+
 import rclpy
 from rclpy.executors import MultiThreadedExecutor
 from clr_control.clr_estop_safety_class import CLREStopSafety
 
+
 def main(args=None):
-    
+
     rclpy.init(args=args)
 
+    clr_estop_node = CLREStopSafety()
+    executor = MultiThreadedExecutor(num_threads=2)
+    executor.add_node(clr_estop_node)
+
     try:
-        clr_estop_node = CLREStopSafety()
-
-        executor = MultiThreadedExecutor(num_threads=2)
-        executor.add_node(clr_estop_node)
-
-        try: 
-            print("spinning")
-            executor.spin()         
-
-        finally:
-            executor.shutdown()
-            clr_estop_node.destroy_node()
-        
+        print("spinning")
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
+    except rclpy.executors.ExternalShutdownException:
+        sys.exit(1)
     finally:
-        rclpy.shutdown()
+        clr_estop_node.destroy_node()
+        executor.shutdown()
+        rclpy.try_shutdown()
 
 
 if __name__ == '__main__':
