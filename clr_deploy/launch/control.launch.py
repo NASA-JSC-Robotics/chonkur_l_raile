e59f03b8120d7485092c75b3643c57112e28adf3
Erik Holum <erik.holum@nasa.gov>
2025-04-24 10:20:12 -0400

    Add standalone launch setup for CLR

---
+++ clr_deploy/launch/control.launch.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, TimerAction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterFile
+from launch.conditions import UnlessCondition
+from chonkur_deploy.launch_helpers import include_launch_file, spawn_controller
+
+
+def generate_launch_description():
+
+    # arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ns",
+            default_value="",
+            description="Namespace for the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value="",
+            description="tf prefix for the robot joints.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "enable_admittance",
+            default_value="false",
+            description="Allow the admittance controllers to spawn",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="true",
+            description="Start robot with simulated hardware mirroring command to its states.",
+        )
+    )
+
+    enable_admittance = LaunchConfiguration("enable_admittance")
+    ns = LaunchConfiguration("ns")
+    tf_prefix = LaunchConfiguration("tf_prefix")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+
+    def param_file(package, config, allow_substs=False):
+        """
+        Return a param file given the specified package and path.
+        """
+        return ParameterFile(
+            PathJoinSubstitution([get_package_share_directory(package), "config", config]),
+            allow_substs=allow_substs,
+        )
+
+    # # start the controller manager node with all of the controller config files
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        namespace=ns,
+        # allow_substs allows tf_prefix to be pulled in
+        parameters=[
+            # CLR specific controllers
+            param_file("clr_deploy", "controllers_common.yaml", True),
+            param_file("clr_deploy", "clr_controllers.yaml", True),
+            # Pulling in default controller configs for the hande, ur10e, lift, and rail
+            param_file("chonkur_deploy", "ur10e_update_rate.yaml", True),
+            param_file("chonkur_deploy", "ur10e_controllers.yaml", True),
+            param_file("chonkur_deploy", "hande_controllers.yaml", True),
+            param_file("ewellix_liftkit_deploy", "liftkit_controllers.yaml", True),
+            param_file("vention_rail_deploy", "rail_controllers.yaml", True),
+        ],
+        remappings=[
+            # remap to be able to use the global robot_description
+            ("~/robot_description", "robot_description"),
+        ],
+        output="both",
+    )
+
+    robot_state_publisher = include_launch_file(
+        package_name="clr_deploy",
+        launch_file="robot_state_publisher.launch.py",
+        launch_arguments={
+            "ns": ns,
+            "tf_prefix": tf_prefix,
+            "use_fake_hardware": use_fake_hardware,
+        }.items(),
+    )
+
+    # Spawn CLR controllers
+    spawn_controllers = include_launch_file(
+        package_name="clr_deploy",
+        launch_file="spawn_controllers.launch.py",
+        launch_arguments={
+            "ns": ns,
+            "tf_prefix": tf_prefix,
+            "use_fake_hardware": use_fake_hardware,
+            "enable_admittance": enable_admittance,
+        }.items(),
+    )
+
+    # Additional tools for the UR10e from UR
+    ur_tools = include_launch_file(
+        package_name="chonkur_deploy",
+        launch_file="ur_tools.launch.py",
+        launch_arguments={
+            "use_fake_hardware": use_fake_hardware,
+        }.items(),
+    )
+
+    # CLR specific joint_state_broadcaster
+    joint_state_broadcaster = spawn_controller("joint_state_broadcaster", namespace=ns)
+
+    # E-stop controller manager for CLR
+    clr_safety_stopper = Node(
+        package="clr_control",
+        executable="clr_estop_node.py",
+        parameters=[
+            param_file("clr_deploy", "consistent_controllers.yaml", True),
+        ],
+        condition=UnlessCondition(use_fake_hardware),
+    )
+
+    # wait for the controller stopper until everything else is loaded so that we can then manage,
+    # instead of coming in during the middle of the loading process
+    delay_controller_stopper = TimerAction(period=10.0, actions=[clr_safety_stopper])
+
+    nodes = [control_node, joint_state_broadcaster, clr_safety_stopper, delay_controller_stopper]
+    launch_files = [robot_state_publisher, spawn_controllers, ur_tools]
+    return LaunchDescription(declared_arguments + nodes + launch_files)
---
+++ clr_deploy/launch/robot_state_publisher.launch.py
@@ -0,0 +1,78 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ns",
+            default_value="",
+            description="Namespace for the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value="",
+            description="tf prefix for the robot joints.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="true",
+            description="Start robot with simulated hardware mirroring command to its states.",
+        )
+    )
+
+    ns = LaunchConfiguration("ns")
+    tf_prefix = LaunchConfiguration("tf_prefix")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+
+    # main robot description for Phoebe
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("clr_description"), "urdf", "clr.urdf.xacro"]),
+            " ",
+            "tf_prefix:=",
+            tf_prefix,
+            " ",
+            "use_fake_hardware:=",
+            use_fake_hardware,
+            " ",
+            "ros2_control_name:=",
+            "clr",
+            " ",
+            "marker_opacity:=",
+            "0.5",
+            " ",
+            "finger_xacro:=",
+            "fngr_v6",
+            " ",
+        ]
+    )
+
+    robot_description = {"robot_description": ParameterValue(value=robot_description_content, value_type=str)}
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        namespace=ns,
+        output="both",
+        parameters=[robot_description],
+    )
+
+    return LaunchDescription(declared_arguments + [robot_state_publisher_node])
