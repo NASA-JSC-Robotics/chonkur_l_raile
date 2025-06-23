e5ebc1a6bfc121937cd3280a5a0f5effb1df98ef
Nikki Hart <nikki.j.hart@nasa.gov>
2025-06-23 15:04:35 -0500

    make clr srdfs modular and add the mockups version to the moveit config. still have to resolve one self collision and make mockups optional

--- clr_moveit_config/launch/clr_moveit.launch.py
+++ clr_moveit_config/launch/clr_moveit.launch.py
@@ -20,7 +20,7 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
@@ -74,8 +74,11 @@ def generate_launch_description():
     launch_rviz = LaunchConfiguration("launch_rviz")
     use_sim_time = {"use_sim_time": LaunchConfiguration("use_sim_time")}
 
-    description_package = "clr_description"
-    description_file = "clr.urdf.xacro"
+    description_package = "clr_imetro_environments"
+    description_file = "clr_trainer_multi_hatch.urdf.xacro"
+    moveit_config_file_path = "srdf/clr_and_mockups.srdf.xacro"
+
+
     description_full_path = os.path.join(get_package_share_directory(description_package), "urdf", description_file)
     moveit_config_package = "clr_moveit_config"
 
@@ -83,7 +86,7 @@ def generate_launch_description():
     moveit_config = (
         MoveItConfigsBuilder("clr", package_name="clr_moveit_config")
         .robot_description(file_path=description_full_path)
-        .robot_description_semantic(file_path="srdf/clr.srdf")
+        .robot_description_semantic(file_path=moveit_config_file_path)
         .robot_description_kinematics(file_path="config/kinematics.yaml")
         .joint_limits(file_path="config/joint_limits.yaml")
         .to_moveit_configs()
