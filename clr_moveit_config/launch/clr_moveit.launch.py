31bce85b190f45e1d33a9600acb18a20237fa845
Nikki Hart <nikki.j.hart@nasa.gov>
2025-06-25 12:06:26 -0500

    clean up

--- clr_moveit_config/launch/clr_moveit.launch.py
+++ clr_moveit_config/launch/clr_moveit.launch.py
@@ -18,9 +18,9 @@
 # under the License.
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration, OpaqueFunction
-from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
