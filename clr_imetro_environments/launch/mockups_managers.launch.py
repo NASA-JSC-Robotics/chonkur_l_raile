0c512e5ba31bf44771bee31693cf2acb347b1419
Nikki Hart <nikki.j.hart@nasa.gov>
2025-08-06 17:27:20 -0500

    add launch file for mockups managers

---
+++ clr_imetro_environments/launch/mockups_managers.launch.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2025, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+#
+# All rights reserved.
+#
+# This software is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, NotSubstitution
+from launch_ros.actions import Node
+
+def launch_setup(context, *args, **kwargs):
+    tf_prefix = LaunchConfiguration("tf_prefix").perform(context)
+
+    nodes_to_start = [
+        Node(
+            package="hatch_4040",
+            executable="hatch4040_manager.py",
+            output="both",
+            condition=IfCondition(LaunchConfiguration("hatch_4040")),
+            parameters=[
+                {"prefix": "lorge/"},
+            ],
+        ),
+        Node(
+            package="trainer",
+            executable="trainer_manager.py",
+            output="both",
+            condition=IfCondition(LaunchConfiguration("trainer")),
+            parameters=[
+                {"prefix": tf_prefix},
+            ],
+        ),
+        Node(
+            package="trainer",
+            executable="trainer_manager.py",
+            name="second_trainer_manager",
+            output="both",
+            condition=IfCondition(LaunchConfiguration("second_trainer")),
+            parameters=[
+                {"prefix": "second_trainer/"},
+            ],
+        ),
+    ]
+
+    return nodes_to_start
+
+def generate_launch_description():
+
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value="",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "hatch_4040",
+            default_value="true",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "trainer",
+            default_value="true",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "second_trainer",
+            default_value="false",
+        )
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
