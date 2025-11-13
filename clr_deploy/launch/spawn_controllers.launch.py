1fb2032aaba7fec2a40738ef6c4aae620125f11c
Erik Holum <erik.holum@nasa.gov>
2025-11-13 09:52:00 -0500

    Replace the servo controller with a VFC

--- clr_deploy/launch/spawn_controllers.launch.py
+++ clr_deploy/launch/spawn_controllers.launch.py
@@ -72,13 +72,11 @@ def generate_launch_description():
     # Load CLR specific controllers
     lift_rail_controller = spawn_controller("lift_rail_joint_trajectory_controller", namespace=namespace, inactive=True)
     clr_controller = spawn_controller("clr_joint_trajectory_controller", namespace=namespace, inactive=True)
-    clr_servo_controller = spawn_controller("servo_controller", namespace=namespace, inactive=True)
     streaming_controller = spawn_controller("streaming_controller", namespace=namespace, inactive=True)
 
     controller_spawners = [
         lift_rail_controller,
         clr_controller,
-        clr_servo_controller,
         streaming_controller,
     ]
 
