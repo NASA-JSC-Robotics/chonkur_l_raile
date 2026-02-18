ed9fe7fac65ee899e36812606e48bca817a5b45a
Mike Tobia <michael.e.tobia@nasa.gov>
2026-02-17 18:25:59 -0600

    more format

--- chonkur/chonkur_deploy/launch/chonkur_gui.launch.py
+++ chonkur/chonkur_deploy/launch/chonkur_gui.launch.py
@@ -22,7 +22,6 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 from chonkur_deploy.launch_helpers import include_launch_file
 from ament_index_python.packages import get_package_share_directory
 
