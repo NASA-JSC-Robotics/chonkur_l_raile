2147a505b30b0a48657dc9a4cdc1535fee9e7d08
Erik Holum <erik.holum@nasa.gov>
2026-04-16 15:27:58 -0500

    Namespacing in CLR

--- clr_deploy/launch/control.launch.py
+++ clr_deploy/launch/control.launch.py
@@ -17,6 +17,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from chonkur_deploy.launch_helpers import (
+    include_launch_file,
+    parameter_file,
+    spawn_controller,
+)
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
@@ -30,11 +35,6 @@ from launch.substitutions import (
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
-from chonkur_deploy.launch_helpers import (
-    include_launch_file,
-    parameter_file,
-    spawn_controller,
-)
 
 
 def generate_launch_description():
@@ -171,7 +171,11 @@ def generate_launch_description():
         ]
     )
 
-    robot_description = {"robot_description": ParameterValue(value=robot_description_content, value_type=str)}
+    robot_description = {
+        "robot_description": ParameterValue(
+            value=robot_description_content, value_type=str
+        )
+    }
 
     # State publisher for CLR. We include here for access to the top level robot_description content.
     # TODO: Separate this out once we are able to load the robot description from a topic in the controller manager.
@@ -186,13 +190,15 @@ def generate_launch_description():
         ],
     )
 
-    # # start the controller manager node with all of the controller config files
+    # Start the controller manager node with all of the controller config files
     control_node = Node(
         package=control_node_package,
         executable="ros2_control_node",
         namespace=namespace,
         # allow_substs allows tf_prefix to be pulled in
         parameters=[
+            {"tf_prefix": tf_prefix},
+            {"use_sim_time": use_sim_time},
             # CLR specific controllers
             parameter_file("clr_deploy", "controllers_common.yaml", True),
             parameter_file("clr_deploy", "clr_controllers.yaml", True),
@@ -201,7 +207,6 @@ def generate_launch_description():
             parameter_file("chonkur_deploy", "hande_controllers.yaml", True),
             parameter_file("ewellix_liftkit_deploy", "liftkit_controllers.yaml", True),
             parameter_file("vention_rail_deploy", "rail_controllers.yaml", True),
-            {"use_sim_time": use_sim_time},
         ],
         output="both",
     )
