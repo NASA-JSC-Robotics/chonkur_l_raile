cfc0af0a8f26890ef63987be492b97f84b49a39d
Nikki Hart <nikki.j.hart@nasa.gov>
2026-07-14 15:48:52 -0500

    first pass at including generate_clr_mjcf.launch.py in clr_mujoco.launch.py

--- clr_mujoco_config/launch/generate_clr_mjcf.launch.py
+++ clr_mujoco_config/launch/generate_clr_mjcf.launch.py
@@ -1,6 +1,10 @@
+import os
+import tempfile
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, RegisterEventHandler
+from launch.event_handlers import OnShutdown
 from launch.substitutions import (
     Command,
     FindExecutable,
@@ -24,9 +28,47 @@ def generate_launch_description():
             description="Use pre-generated assets dir. This is useful if you are just modifying an existing structure",
         )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "mujoco_launch",
+            default_value="false",
+            description="True if this launch file is included in clr_mujoco.launch.py.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "include_scene_objects",
+            default_value="true",
+            description="Whether to include scene objects.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "model_env",
+            default_value="true",
+            description="Whether to model the iMETRO environment.",
+        )
+    )
+
+    include_scene_objects = LaunchConfiguration("include_scene_objects")
+    model_env = LaunchConfiguration("model_env")
 
     clr_mujoco_package_name = "clr_mujoco_config"
     clr_mujoco_description_file = "clr_mujoco_xacro.urdf"
+
+    generate_arguments = [
+        "--save_only",
+    ]
+
+    mujoco_launch_arguments = [
+        "--publish_topic",
+        "/mujoco_robot_description",
+    ]
+
+    assets_dir = [
+        "--asset_dir",
+        PathJoinSubstitution([FindPackageShare(clr_mujoco_package_name), "description", "assets"]),
+    ]
 
     # Main robot description for CLR
     robot_description_content = Command(
@@ -34,52 +76,50 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution([FindPackageShare(clr_mujoco_package_name), "urdf", clr_mujoco_description_file]),
-            " ",
             # Grasp frames should not be converted to MJCF objects
-            "add_grasp_push_frames:=",
-            "false",
-            " ",
-            "model_env:=",
-            "true",
-            " ",
-            "include_scene_objects:=",
-            "true",
+            " add_grasp_push_frames:=false",
+            " model_env:=",
+            model_env,
+            " include_scene_objects:=",
+            include_scene_objects,
         ]
     )
 
-    default_arguments = [
-        "--robot_description",
-        robot_description_content,
-        "--convert_stl_to_obj",
-        "--save_only",
-    ]
+    # Using an inline opaque function to write the URDF for mujoco to a tempfile...
+    # This prevents it from being dumped into the console on conversion errors.
+    def launch_mjcf_node(context):
+        tmp = tempfile.NamedTemporaryFile(mode="w", suffix=".urdf", delete=False)
+        tmp.write(robot_description_content.perform(context))
+        tmp.close()
 
-    args_with_assets_dir = default_arguments + [
-        "--asset_dir",
-        PathJoinSubstitution([FindPackageShare(clr_mujoco_package_name), "description", "assets"]),
-    ]
+        # Ensure the file gets deleted
+        def cleanup(event, context):
+            if os.path.exists(tmp.name):
+                os.remove(tmp.name)
 
-    # this version can be run for general
-    make_mjcf_from_robot_description = Node(
-        package="mujoco_ros2_control",
-        executable="make_mjcf_from_robot_description.py",
-        output="screen",
-        arguments=default_arguments,
-        condition=UnlessCondition(LaunchConfiguration("use_pregenerated_assets_dir")),
-    )
+        args = ["--urdf", tmp.name, "--convert_stl_to_obj",]
 
-    make_mjcf_from_robot_description_use_assets_dir = Node(
-        package="mujoco_ros2_control",
-        executable="make_mjcf_from_robot_description.py",
-        output="screen",
-        arguments=args_with_assets_dir,
-        condition=IfCondition(LaunchConfiguration("use_pregenerated_assets_dir")),
-    )
+        if LaunchConfiguration("mujoco_launch").perform(context) == "true":
+            args += mujoco_launch_arguments
+        else:
+            args += generate_arguments
+        if LaunchConfiguration("use_pregenerated_assets_dir").perform(context) == "true":
+            args += assets_dir
+        
+        return [
+            Node(
+                package="mujoco_ros2_control",
+                executable="make_mjcf_from_robot_description.py",
+                output="both",
+                emulate_tty=True,
+                arguments=args,
+            ),
+            RegisterEventHandler(OnShutdown(on_shutdown=cleanup)),
+        ]
+
+    generate_mjcf = OpaqueFunction(function=launch_mjcf_node)
 
     return LaunchDescription(
         declared_arguments
-        + [
-            make_mjcf_from_robot_description,
-            make_mjcf_from_robot_description_use_assets_dir,
-        ]
+        + [generate_mjcf]
     )
