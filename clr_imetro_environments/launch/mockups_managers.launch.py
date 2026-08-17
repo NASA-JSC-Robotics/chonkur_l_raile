ae85f3d018ef5e3447a87b2caea47ba1f5fdd815
Nathan Dunkelberger <nathan.b.dunkelberger@nasa.gov>
2026-08-17 14:20:52 -0500

    pre-commit

--- clr_imetro_environments/launch/mockups_managers.launch.py
+++ clr_imetro_environments/launch/mockups_managers.launch.py
@@ -18,9 +18,7 @@
 # under the License.
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
